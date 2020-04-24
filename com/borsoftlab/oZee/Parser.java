8377a62fb80749f6f60172953392ba1d8f03b327
Igor Borozdin <igorbor@inbox.ru>
2020-04-24 11:52:24 +0400

    Added Parser class with stub

---
+++ com/borsoftlab/oZee/Parser.java
@@ -0,0 +1,13 @@
+package com.borsoftlab.oZee;
+
+public class Parser{
+    
+    public void Parser(){
+
+    }
+
+    public void compile(){
+
+    }
+
+}
--- com/borsoftlab/oZee/oZee.java
+++ com/borsoftlab/oZee/oZee.java
@@ -3,5 +3,8 @@ package com.borsoftlab.oZee;
 public class oZee {
     public static void main(final String[] args) {
         System.out.println("\n...oZee compiler...");
+        Parser parser = new Parser();
+        parser.compile();
+
     }    
 }
