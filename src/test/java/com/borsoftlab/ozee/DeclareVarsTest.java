e3a0b3d64ba2946916ce9d6389be3b56edcb56e2
Igor Borozdin <igorbor@inbox.ru>
2020-05-07 16:24:21 +0400

    Удален ненужный импорт

--- src/test/java/com/borsoftlab/ozee/DeclareVarsTest.java
+++ src/test/java/com/borsoftlab/ozee/DeclareVarsTest.java
@@ -1,11 +1,8 @@
 package com.borsoftlab.ozee;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsProvider;
-import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
