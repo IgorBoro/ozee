d432b425b8b4e5624b6bb2355bd213dc4bb7157c
Igor Borozdin <igorbor@inbox.ru>
2020-05-10 15:55:26 +0400

    Добавлен тест вычисления выражений ExpressionTest

---
+++ src/test/java/com/borsoftlab/ozee/ExpressionTest.java
@@ -0,0 +1,65 @@
+package com.borsoftlab.ozee;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.stream.Stream;
+
+@Nested
+@DisplayName("Test class")
+public class ExpressionTest {
+
+    final static String program0 
+                        = "float g = 187 * 10.0;"    + '\n'
+                        + "float p = 7.7;"           + '\n'
+                        + "float f = 0.523 * 12.3 + g - 41.6/32 * (p + 76);";
+    final static String message0
+                        = "Ok";
+
+
+    OzParser parser   = new OzParser();
+    OzScanner scanner = new OzScanner();
+
+    @ParameterizedTest(name="{index}")
+    @MethodSource("argumentProvider")
+    public void test(String program, String message) {
+        System.out.println("::------------------------------------------::");
+        try {     
+            final InputStream programStream = new ByteArrayInputStream(program.getBytes());
+            try {
+                final OzText text = new OzText(programStream);
+                scanner.resetText(text);
+                parser.compile(scanner);
+                scanner.symbolTable.dumpSymbolTable();
+            } catch (final Exception e) {
+            } finally {
+                System.out.println(OzCompileError.messageString);
+                try {
+                    programStream.close();
+                } catch (final IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        assertEquals(message, OzCompileError.messageString.toString());
+    }
+
+    private static Stream<Arguments> argumentProvider() {
+        return Stream.of(
+            Arguments.of( program0, message0 )
+        );
+    }
+}
+
+
+    
