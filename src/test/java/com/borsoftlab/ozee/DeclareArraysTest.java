2f095decbe804289d991ffe164b8243c3c11e7df
Igor Borozdin <igorbor@inbox.ru>
2020-06-01 16:28:14 +0400

    Упорядочивание тестов с объявлением массивов

--- src/test/java/com/borsoftlab/ozee/DeclareArraysTest.java
+++ src/test/java/com/borsoftlab/ozee/DeclareArraysTest.java
@@ -46,7 +46,6 @@ public class DeclareArraysTest {
     final static String message3
                         = "Ok";
 
-
     final static String program4
                         = "int[] i = 15;";
     final static String message4
@@ -61,23 +60,44 @@ public class DeclareArraysTest {
                         = "Ok";
 
     final static String program6
+                        = "int[] i = int[15];";
+    final static String message6
+                        = "Ok";
+
+    final static String program7
                         = "int i;" + "\n"
                         + "i = int[15];";
-    final static String message6
+    final static String message7
                         = "i = int[15];" + "\n"
                         + "    ^"        + "\n"
                         + "Error in line 2: expected scalar type" + "\n";
 
-    final static String program7
+    final static String program8 
+                        = "int i = int[15];";
+    final static String message8
+                        = "int i = int[15];" + "\n"
+                        + "        ^"        + "\n"
+                        + "Error in line 1: expected scalar type" + "\n";
+
+    final static String program9
                         = "int[] i;" + "\n"
                         + "i = short[15];";
-    final static String message7
+    final static String message9
                         = "i = short[15];" + "\n"
                         + "    ^"          + "\n"
                         + "Error in line 2: incompatible array types" + "\n";
-                        
 
 // -----------------------------------------------------------------------                        
+
+
+    final static String program10 
+                        = "int[] i = int[15];";
+    final static String message10
+                        = "int i = int[15];" + "\n"
+                        + "        ^"        + "\n"
+                        + "Error in line 1: expected scalar type" + "\n";
+                        
+
 
     OzParser parser   = new OzParser();
     OzScanner scanner = new OzScanner();
@@ -117,10 +137,10 @@ public class DeclareArraysTest {
             Arguments.of( program4, message4 ),
             Arguments.of( program5, message5 ),
             Arguments.of( program6, message6 ),
-            Arguments.of( program7, message7 )
-//            Arguments.of( program8, message8 ),
-//            Arguments.of( program9, message9 ),
-//            Arguments.of( program10, message10 ),
+            Arguments.of( program7, message7 ),
+            Arguments.of( program8, message8 ),
+            Arguments.of( program9, message9 )
+//            Arguments.of( program10, message10 )
 //            Arguments.of( program11, message11 ),
 //            Arguments.of( program12, message12 ),
 //            Arguments.of( program13, message13 ),
