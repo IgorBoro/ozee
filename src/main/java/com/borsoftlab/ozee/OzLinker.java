c29ae7e730dbd27913c10dab6b2837f9a28408de
Igor Borozdin <igorbor@inbox.ru>
2020-06-01 09:36:38 +0400

    Добавлен заголвок при линковке и его пропуск при загрузке

--- src/main/java/com/borsoftlab/ozee/OzLinker.java
+++ src/main/java/com/borsoftlab/ozee/OzLinker.java
@@ -7,26 +7,29 @@ import com.borsoftlab.ozee.OzSymbols.Symbol;
 public class OzLinker {
 
         public static byte[] linkImage(final byte[] program, final List<Symbol> symbols){
+                int headerSize = 0;
+                int codeOriginAddress = 4 + headerSize;
+
                 int progSize = program.length;     
                 int symbolTableSize = 0;
 
                 for (Symbol symbol : symbols) {
+                        symbol.allocAddress += ( codeOriginAddress + progSize );        
                         symbolTableSize += symbol.sizeInBytes;
                 }
 
-                int imageSize = progSize + symbolTableSize;
+                int imageSize = codeOriginAddress + progSize + symbolTableSize;
                 byte[] image = new byte[imageSize];
-
-                for (int i = 0; i < progSize; i++) {
-                        image[i] = program[i];
+                OzUtils.storeIntToByteArray(image, 0, codeOriginAddress);
+                for (int i = codeOriginAddress; i < progSize + codeOriginAddress; i++) {
+                        image[i] = program[i - codeOriginAddress];
                 }
 
                 // re-binding refs
                 for (Symbol symbol : symbols) {
-                        symbol.allocAddress += progSize;        
                         List<Integer> refList = symbol.refList;
                         for (Integer ref : refList) {
-                                OzUtils.storeIntToByteArray(image, ref, symbol.allocAddress);        
+                                OzUtils.storeIntToByteArray(image, ref + codeOriginAddress, symbol.allocAddress);        
                         }
                 }
                 return image;
--- src/main/java/com/borsoftlab/ozee/OzVm.java
+++ src/main/java/com/borsoftlab/ozee/OzVm.java
@@ -120,7 +120,7 @@ public class OzVm{
     }
 
     public void execute() throws Exception {
-        pc = 0;
+        pc = OzUtils.fetchIntFromByteArray(ram, 0);
         sp = 0; // the stack is growing up
 
         byte cmd = ram[pc];
