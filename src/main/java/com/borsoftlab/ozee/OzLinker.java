eb278c2e0e1223227700da6d0c55a7b4f179d53b
Igor Borozdin <igorbor@inbox.ru>
2020-06-02 12:32:58 +0400

    Дописаны комментарии, подготовлен код для инициализации сенмента данных

--- src/main/java/com/borsoftlab/ozee/OzLinker.java
+++ src/main/java/com/borsoftlab/ozee/OzLinker.java
@@ -7,30 +7,53 @@ import com.borsoftlab.ozee.OzSymbols.Symbol;
 public class OzLinker {
 
         public static byte[] linkImage(final byte[] program, final List<Symbol> symbols){
+
+                // a header of image is empty
                 int headerSize = 0;
                 int codeOriginAddress = 4 + headerSize;
 
                 int progSize = program.length;     
                 int symbolTableSize = 0;
 
+                // calculate image size
                 for (Symbol symbol : symbols) {
                         symbol.allocAddress += ( codeOriginAddress + progSize );        
                         symbolTableSize += symbol.sizeInBytes;
                 }
-
                 int imageSize = codeOriginAddress + progSize + symbolTableSize;
+
+                // create the empty image
                 byte[] image = new byte[imageSize];
+
+                // put the start address at the beginning of the image
                 OzUtils.storeIntToByteArray(image, 0, codeOriginAddress);
+
+                // copy the program to the image
                 for (int i = codeOriginAddress; i < progSize + codeOriginAddress; i++) {
                         image[i] = program[i - codeOriginAddress];
                 }
 
+                // initialize the data section
+                               
                 // re-binding refs
                 for (Symbol symbol : symbols) {
-                        List<Integer> refList = symbol.refList;
-                        for (Integer ref : refList) {
-                                OzUtils.storeIntToByteArray(image, ref + codeOriginAddress, symbol.allocAddress);        
-                        }
+                    List<Integer> refList = symbol.refList;
+                    /*
+                    switch(symbol.sizeInBytes){
+                        case 4:
+                            OzUtils.storeIntToByteArray(image, progSize + codeOriginAddress + symbol.allocAddress, symbol.value);        
+                            break;
+                        case 2:
+                            OzUtils.storeShortToByteArray(image, progSize + codeOriginAddress + symbol.allocAddress, symbol.value);        
+                            break;
+                        case 1:
+                            OzUtils.storeByteToByteArray(image, progSize + codeOriginAddress + symbol.allocAddress, symbol.value);        
+                            break;
+                    }
+                    */
+                    for (Integer ref : refList) {
+                        OzUtils.storeIntToByteArray(image, ref + codeOriginAddress, symbol.allocAddress);        
+                    }
                 }
                 return image;
         }
